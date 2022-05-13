eea8ea724dcf893207675486a79dd38b1ca75d4e
Jihun Kim <Jihun.Dev@gmail.com>
2022-05-13 17:01:42 +0900

    modfiy url pattern
    - warnings log
    ?: (2_0.W001) Your URL pattern '^swagger(?P<format>\.json|\.yaml)$' [name='schema-json'] has a route that contains '(?P<', begins with a '^', or ends with a '$'. This was likely an oversight when migrating to django.urls.path().

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -28,7 +28,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/^format\.json|\.yaml', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
